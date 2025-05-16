a0be903c15311e39fc479f72010e7e261d5852a9
Abhik <theabhik2020@gmail.com>
2025-05-16 10:35:32 +0530

    main: Added

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.get('/api/hello', (req: Request, res: Response) => {
     res.json({ message: 'Hello from plain Express app!' });
 });
-
+//test
 // app.listen(PORT, () => {
 //     console.log(`Server is running on http://localhost:${PORT}`);
 // });
